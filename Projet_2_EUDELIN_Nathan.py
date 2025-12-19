1e8d86d86f748c0e87d27ddad6ecea4db807f683
Nathan-bit31 <eudelinnathan@gmail.com>
2025-12-19 10:01:59 +0100

    Début dictionnaire

--- Projet_2_EUDELIN_Nathan.py
+++ Projet_2_EUDELIN_Nathan.py
@@ -1,1 +1,34 @@
-print('salut')
+FICHIER_CONTACTS="contacts.txt"
+def charger_contact():
+    contacts=[]
+    try:
+        with open(FICHIER_CONTACTS, "r", encoding='utf-8')as fichier:
+            contact = {
+                "nom":nom,
+                "prenom":prenom,
+                "tel":tel,
+                "email":email,
+            }
+            contacts.append(contact)
+    except FileNotFoundError:
+        print()
+
+def telephone_valide(tel):
+    return len(tel)==10
+
+
+def ajouter_contact(contacts):
+    nom=input("Nom :").upper
+    prenom=input("Prénom :")
+    tel=input("Numéro de téléphone :")
+    while not telephone_valide(tel):
+        tel=input("Le numéro est invalide, réessayer :")
+
+
+
+
+
+
+
+
+
